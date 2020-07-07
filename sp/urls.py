08581cb09b5599c9c1dbb13367b198f6863a4d50
hkj96 <62528203+HKJ96@users.noreply.github.com>
2020-07-07 23:01:43 +0800

    initial commit

---
+++ sp/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import (
+    include,
+    url,
+)
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^saml2/', include('djangosaml2.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^protected-resource/', login_required(TemplateView.as_view(template_name="protected.html"))
+    ),
+]
